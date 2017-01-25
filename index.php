0873c4769029d457aa2d7ab09f8ec3075c2b7774
mhretaab <ermu96@gmail.com>
2017-01-24 16:43:52 -0800

    function that adds numbers created

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
 	echo "first release finished";
 	echo "second release is modifying index page";
 	echo "second releas is finished";
+	
+	
+	
+	function addNumbers(){
+		return 1 + 2;
+	}
+	
+	echo addNumbers();
 ?>
